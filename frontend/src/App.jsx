adbc512ac48a7b4f84a267348b6dce9d3a436696
trytocode <annguyendang.17.07.2002@gmail.com>
2026-02-06 15:04:10 +0700

    refactor folder fe

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import './App.css'
-import { Route, Routes } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
 
 import DefaultLayout from "./layouts/DefaultLayout/DefaultLayout.jsx";
 import LogIn from "./pages/LogIn/LogIn.jsx";
+import {ToastContainer} from "react-toastify";
+import {Route, Routes} from "react-router-dom";
 import Register from "./pages/Register/Register.jsx";
 import Home from "./pages/Home/Home.jsx";
 import Detail from "./pages/Detail/Detail.jsx";
@@ -11,32 +11,26 @@ import PaymentForm from "./pages/PaymentForm/PaymentForm.jsx";
 import PaymentSuccess from "./pages/PaymentResult/PaymentSuccess/PaymentSuccess.jsx";
 import PaymentFail from "./pages/PaymentResult/PaymentFail/PaymentFail.jsx";
 
+
 function App() {
+
     return (
-        <>
-            <Routes>
-                {/* Không dùng layout */}
-                <Route path="/" element={<LogIn />} />
-                <Route path="register" element={<Register />} />
+        <div>
+            <DefaultLayout>
+                <Routes>
+                    <Route path={"/"} element={<LogIn/>}/>
+                    <Route path={"/register"} element={<Register/>}/>
+                    <Route path={"/home"} element={<Home/>}/>
+                    <Route path={"/detail/:id"} element={<Detail/>}/>
+                    <Route path={"/payForm/:info"} element={<PaymentForm/>}/>
+                    <Route path={"/paymentSuccess"} element={<PaymentSuccess/>}/>
+                    <Route path={"/paymentFail"} element={<PaymentFail/>}/>
+                </Routes>
+            </DefaultLayout>
+            <ToastContainer position="top-right" theme="colored" autoClose="2000" closeOnClick="true"/>
 
-                {/* Dùng layout */}
-                <Route element={<DefaultLayout />}>
-                    <Route path="home" element={<Home />} />
-                    <Route path="detail/:id" element={<Detail />} />
-                    <Route path="payForm/:info" element={<PaymentForm />} />
-                    <Route path="paymentSuccess" element={<PaymentSuccess />} />
-                    <Route path="paymentFail" element={<PaymentFail />} />
-                </Route>
-            </Routes>
-
-            <ToastContainer
-                position="top-right"
-                theme="colored"
-                autoClose={2000}
-                closeOnClick
-            />
-        </>
-    );
+        </div>
+    )
 }
 
-export default App;
+export default App
--- frontend/src/layouts/DefaultLayout/DefaultLayout.jsx
+++ frontend/src/layouts/DefaultLayout/DefaultLayout.jsx
@@ -1,12 +1,13 @@
 import Header from "../components/Header/Header.jsx";
 import Footer from "../components/Footer/Footer.jsx";
-import {Outlet} from "react-router-dom";
 
 const DefaultLayout = ({children}) => {
     return (
         <div className={"w-[100%]"}>
             <Header/>
-            <Outlet/>
+            <div className={"flex lg:items-center lg:justify-center lg:mt-[120px]"}>
+                {children}
+            </div>
             <Footer/>
         </div>
     )
