905a63733f8631c04c7b17e029e39da9c0102069
trytocode <annguyendang.17.07.2002@gmail.com>
2026-02-11 12:38:28 +0700

    update page fe

--- frontend/src/pages/About/About.jsx
+++ frontend/src/pages/About/About.jsx
@@ -1,27 +1,8 @@
 import homePic1 from "../../assets/img/home_image_1.png";
 import homePic2 from "../../assets/img/home_image_2.png";
-import {ErrorMessage, Field, Form, Formik} from "formik";
-import {Button} from "@headlessui/react";
-import * as Yup from "yup";
 
 
 const About = () => {
-
-    const formInit = {
-        fullName: "",
-        email: "",
-        message: ""
-    }
-
-    const handleSendContact = (value) => {
-
-    }
-
-    const validationInit = Yup.object({
-        fullName: Yup.string().required("Please enter your full name!"),
-        email: Yup.string().required("Please enter your email!").email("Invalid email format!"),
-        message: Yup.string().required("Please enter your message!")
-    })
     return (<div className={"lg:px-[80px] lg:w-[100%]"}>
         <h2 className={"lg:text-[30px] font-bold lg:text-start lg:w-[100%] text-gray-900"}>ABOUT US</h2>
         <p className={"text-gray-600"}>
@@ -77,36 +58,6 @@ const About = () => {
                         <p className={"lg:text-gray-600"}>079 450 4074</p>
                     </div>
                 </div>
-
-                <Formik initialValues={formInit} onSubmit={handleSendContact} validationSchema={validationInit}>
-                    <Form>
-                        <div>
-                            <p className={"lg:font-bold"}>Full name</p>
-                            <Field name={"fullName"}
-                                   className={"lg:border border-gray-500 lg:w-[100%] rounded-2xl lg:h-[50px] lg:p-3 lg:mt-2"}/>
-                            <ErrorMessage name={"fullName"} component={"small"} className={"text-red-800 font-bold"}/>
-                        </div>
-
-                        <div className={"lg:mt-3"}>
-                            <p className={"lg:font-bold"}>Email address</p>
-                            <Field name={"email"}
-                                   className={"lg:border border-gray-500 lg:w-[100%] rounded-2xl lg:h-[50px] lg:p-3 lg:mt-2"}/>
-                            <ErrorMessage name={"email"} component={"small"} className={"text-red-800 font-bold"}/>
-                        </div>
-
-                        <div className={"lg:mt-3"}>
-                            <p className={"lg:font-bold"}>Message</p>
-                            <Field name={"message"} as="textarea"
-                                   className={"lg:border border-gray-500 lg:w-[100%] rounded-2xl lg:min-h-[100px] lg:p-3 lg:mt-2"}/>
-                            <ErrorMessage name={"message"} component={"small"} className={"text-red-800 font-bold"}/>
-                        </div>
-
-                        <Button type={"submit"}
-                            className={"bg-gray-900 text-white lg:w-[100px] lg:h-[50px] rounded-2xl hover:bg-gray-700 cursor-pointer"}>Send
-                        </Button>
-                    </Form>
-
-                </Formik>
             </div>
         </div>
     </div>)
